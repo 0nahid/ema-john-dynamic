66e7a45d03a2b8104f329746dee3e2e077495245
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-03-15 15:10:05 +0600

    connected to database

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,27 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+require("dotenv").config();
+
 // middleware
 app.use(express.json());
 app.use(cors());
+
+// connect database
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@products.6aa06.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  err ? console.log(err) : console.log("Connected to Database");
+//   client.close();
+});
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
